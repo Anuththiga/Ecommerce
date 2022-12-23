f764029c78cbdd9645f135e71f933010d7719021
Tanuththiga <anuththiga25jt@gmail.com>
2022-12-23 15:17:38 +0530

    user page created

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,6 @@ use App\Http\Controllers\HomeController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
@@ -29,3 +25,5 @@ Route::middleware([
 });
 
 Route::get('/redirect', [HomeController::class, 'redirect']);
+Route::get('/', [HomeController::class, 'index']);
+
